3976b1e02fcd8364a03bac545414dcfba0570960
Evan Bailey <baileyevang@gmail.com>
2018-08-15 23:23:35 -0700

    load haikus and add them to playlist

--- client/index.ts
+++ client/index.ts
@@ -65,22 +65,64 @@ function handleAPILoaded() {
   $("#playlist-button").prop("disabled", false);
 }
 
+function createPlaylist(callback: (playlistId: string) => void) {
+  var request = gapi.client.youtube.playlists.insert({
+    part: 'snippet,status',
+    resource: {
+      snippet: {
+        title: 'Test Playlist',
+        description: 'A private playlist created with the YouTube API'
+      },
+      status: {
+        privacyStatus: 'private'
+      }
+    }
+  });
+  request.execute(function (response) {
+    var result = response.result;
+    if (result) {
+      callback(result.id);
+    } else {
+      $('#status').html('Could not create playlist');
+    }
+  });
+}
+
+// Add a video to a playlist. The "startPos" and "endPos" values let you
+// start and stop the video at specific times when the video is played as
+// part of the playlist. However, these values are not set in this example.
+function addToPlaylist(playlistId, videoId, startPos?, endPos?) {
+  var details = {
+    videoId,
+    kind: 'youtube#video'
+  }
+  if (startPos != undefined) {
+    details['startAt'] = startPos;
+  }
+  if (endPos != undefined) {
+    details['endAt'] = endPos;
+  }
+  var request = gapi.client.youtube.playlistItems.insert({
+    part: 'snippet',
+    resource: {
+      snippet: {
+        playlistId,
+        resourceId: details
+      }
+    }
+  });
+  request.execute(function (response) {
+    $('#status').html('<pre>' + JSON.stringify(response.result) + '</pre>');
+  });
+}
+
+
 $.getJSON('/youtubehaiku/top', (ids: string[]) => {
   $('.play')
     .prop("disabled", false)
     .click(() => {
-      const request = gapi.client.youtube.playlists.insert({
-        part: 'snippet,status',
-        resource: {
-          snippet: {
-            title: 'Test Playlist',
-            description: 'A private playlist created with the YouTube API'
-          },
-          status: {
-            privacyStatus: 'private'
-          }
-        }
+      createPlaylist(playlistId => {
+        ids.forEach(id => addToPlaylist(playlistId, id));
       });
-
     });
 }, err => console.error(err));
